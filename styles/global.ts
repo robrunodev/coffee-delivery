e59269d69cc64480487da6b16b8caf8d37e7ad59
Rodrigo Bruno <ro_web1@hotmail.com>
2024-09-09 09:20:38 -0300

    feat: Add DefaultLayout component and styles
    - Added DefaultLayout component to provide a default layout for the app
    - Created styles for the DefaultLayout component

---
+++ styles/global.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+*{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+
+    :focus {
+        outline: 0;
+        box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+    }
+
+    body{
+        background-color: ${(props) => props.theme['gray-900']};
+        color: ${(props) => props.theme['gray-300']};
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: "Roboto", sans-serif;
+        font-weight: 400;
+        font-size: 1rem;
+    }
+
+    .baloo-2--bold {
+        font-family: "Baloo 2", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 400;
+        font-style: normal;
+    }
+    
+    .baloo-2--extra-bold {
+        font-family: "Baloo 2", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 800;
+        font-style: normal;
+    }
+
+`
