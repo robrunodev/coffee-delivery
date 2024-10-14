ccb3d4af1b8db6c4160db57f1396d4822c0094b8
robrunodev <ro_web1@hotmail.com>
2024-10-14 08:12:34 -0300

    feat: Add Radio component with styles

---
+++ src/components/Radio/index.tsx
@@ -0,0 +1,18 @@
+import { forwardRef, InputHTMLAttributes, LegacyRef } from 'react'
+import { Container } from './styles'
+
+type RadioProps = InputHTMLAttributes<HTMLInputElement> & {
+    isSelected: boolean
+}
+
+export const Radio = forwardRef(function Radio(
+    { children, isSelected, ...rest }: RadioProps,
+    ref: LegacyRef<HTMLInputElement>
+) {
+    return (
+        <Container data-state={isSelected}>
+            <input type="radio" ref={ref} {...rest} />
+            {children}
+        </Container>
+    )
+}) 
